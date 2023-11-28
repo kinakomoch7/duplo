5c06888715dfbb68d125fbf088e438bd91ada04f
kinakomoch7 <book-insect14@outlook.jp>
2023-11-28 22:49:28 +0900

    [add]コンポーネント作成・muiのダウンロード

---
+++ src/constants/path.ts
@@ -0,0 +1,7 @@
+export const HOME_PATH = '/';
+export const REGISTER_PATH = '/Register';
+export const MENU_PATH = '/Menu';
+
+export const HOME = 'HOME';
+export const REGISTER = 'REGISTER';
+export const MENU = 'MENU';
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Reset.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './Pages/Home/Home';
+import { Register } from './Pages/Register/Register';
+import { NotFound } from './Pages/NotFound/NotFound';
+import { Menu } from './Pages/Menu/Menu';
+import { HOME_PATH, MENU_PATH, REGISTER_PATH } from './constants/path';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes >
+        <Route path={HOME_PATH} element={ <Home /> } />
+        <Route path={REGISTER_PATH} element={ <Register /> } />
+        <Route path={MENU_PATH} element={ <Menu /> } />
+        <Route path='*' element={ <NotFound /> } />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
