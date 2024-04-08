686c22a29c68861457fabf613bfaa53bc0c874e7
k_tomoki <114901771+kinakomoch7@users.noreply.github.com>
2024-04-08 22:29:10 +0900

    [update]PWA対応しダウンロード可能にした

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
     }),
     VitePWA({
       registerType: 'autoUpdate',
@@ -19,19 +21,19 @@ export default defineConfig({
         description: "duplo",
         icons: [
           {
-            src: "./public/images/vite.svg",
-            type: "image/svg",
+            src: "./public/images/ios/192.png",
+            type: "image/png",
             sizes: "192x192"
           },
           {
-            src: "./public/images/vite.svg",
+            src: "./public/images/ios/512.png",
             sizes: "512x512",
-            type: "image/svg"
+            type: "image/png"
           },
           {
-            src: "./public/images/vite.svg",
+            src: "./public/images/ios/512.png",
             sizes: "512x512",
-            type: "image/svg",
+            type: "image/png",
             purpose: "any maskable"
           }
         ],
